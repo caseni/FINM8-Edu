aa6eb89a079150c08419d7117b86d975690f87e9
caseni <84281675+caseni@users.noreply.github.com>
2026-07-15 22:14:56 +0300

    feat: export compact learning preferences

--- src/components/learning/index.ts
+++ src/components/learning/index.ts
@@ -2,5 +2,6 @@ export * from './LessonBlockRenderer';
 export * from './LessonPlayer';
 export * from './LearningVisual';
 export * from './LearningModuleCard';
+export * from './LearningPreferencesCard';
 export * from './PracticalTaskPlayer';
 export * from './QuizPlayer';
