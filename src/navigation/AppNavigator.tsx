d54b4310e80ff501e8cb0288a24d49238b3e0622
caseni <84281675+caseni@users.noreply.github.com>
2026-07-15 19:34:50 +0300

    Wave 1: update AppNavigator.tsx

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -9,6 +9,7 @@ import { LearningOnboardingScreen } from '../screens/learn/LearningOnboardingScr
 import { MicroLessonScreen } from '../screens/learn/MicroLessonScreen';
 import { LessonQuizScreen } from '../screens/learn/LessonQuizScreen';
 import { PracticalTaskScreen } from '../screens/learn/PracticalTaskScreen';
+import { MarketFoundationsChallengeScreen } from '../screens/learn/MarketFoundationsChallengeScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -38,6 +39,7 @@ export const AppNavigator: React.FC = () => {
         <Stack.Screen name="MicroLesson" component={MicroLessonScreen} options={{ headerShown: false }} />
         <Stack.Screen name="PracticalTask" component={PracticalTaskScreen} options={{ headerShown: false, gestureEnabled: false }} />
         <Stack.Screen name="LessonQuiz" component={LessonQuizScreen} options={{ headerShown: false, gestureEnabled: false }} />
+        <Stack.Screen name="MarketFoundationsChallenge" component={MarketFoundationsChallengeScreen} options={{ headerShown: false, gestureEnabled: false }} />
         <Stack.Screen name="LegacyCatalog" component={HomeScreen} options={{ title: 'Eski Kurs Arşivi' }} />
         <Stack.Screen
           name="CourseDetail"
