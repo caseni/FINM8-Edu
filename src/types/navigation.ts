205f924dafe0c0df3462c7d5a450fe8c45ecd614
caseni <84281675+caseni@users.noreply.github.com>
2026-08-12 18:02:14 +0300

    feat(edu): add public Learn routes

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -9,6 +9,8 @@ interface CurrentQuizReviewRouteContext {
 export type RootStackParamList = {
   Home: undefined;
   Academy: undefined;
+  PublicLearnIndex: undefined;
+  PublicLearn: { slug: string };
   LearningOnboarding: undefined;
   MicroLesson: {
     lessonId: string;
@@ -33,6 +35,8 @@ export type AuthStackParamList = {
 export type MainStackParamList = {
   Home: undefined;
   Academy: undefined;
+  PublicLearnIndex: undefined;
+  PublicLearn: { slug: string };
   LearningOnboarding: undefined;
   MicroLesson: {
     lessonId: string;
